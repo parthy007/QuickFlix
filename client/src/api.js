fc706e8aa53578cc50c0594463188fd6e73a57fc
Parth <parthadhikari1812@gmail.com>
2023-08-11 01:55:19 +0530

    Change in backend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 const rootUrl = process.env.NODE_ENV === 'production' ? 
-'https://quickflix-backend.vercel.app' : 'http://localhost:3001/api';
+'https://quickflix-backend.vercel.app/api' : 'http://localhost:3001/api';
 
 export default rootUrl;
